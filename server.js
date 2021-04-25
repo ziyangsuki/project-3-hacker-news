b2cac74d85e6983adf91cdcdf0e5f2789483bba9
hsinyulu <hsinyu.ken.lu@gmail.com>
2021-04-24 18:01:25 -0700

    new Atlas mongo endpoint

--- server.js
+++ server.js
@@ -14,18 +14,17 @@ app.use(cookieParser());
 // DB Setting Start---------------------------------------------------------
 const mongoose = require('mongoose');
 
-// This is the default address for MongoDB.
-// Make sure MongoDB is running!
-const mongoEndpoint = 'mongodb://127.0.0.1/project_3_hacker_news';
+//Local
+//const mongoEndpoint = 'mongodb://127.0.0.1/project_3_hacker_news';
+
+//Atlas
+const mongoEndpoint = "mongodb+srv://project-3-hacker-news:project-3-hacker-news@project-3-hacker-news.altgv.mongodb.net/project-3-hacker-news?retryWrites=true&w=majority";
 
 // useNewUrlParser is not required, but the old parser is deprecated
 mongoose.connect(mongoEndpoint, { useNewUrlParser: true });
-
-// Get the connection string
 const db = mongoose.connection;
-
-// This will create the connection, and throw an error if it doesn't work
 db.on('error', console.error.bind(console, 'Error connecting to MongoDB:'));
+
 // DB Setting End-----------------------------------------------------------
 
 // ---------------Login Page API---------------
