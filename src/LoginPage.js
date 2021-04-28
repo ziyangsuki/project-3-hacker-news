316fd99fd16f803bf7d17f8616102efe9c1058da
quarkonic <alexistongyy@outlook.com>
2021-04-27 20:39:46 -0700

    edit login page style

--- src/LoginPage.js
+++ src/LoginPage.js
@@ -5,42 +5,42 @@ import './LoginPage.css';
 
 
 class LoginPage extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-        account: "",
-        password: "",
-        error:""
+      account: "",
+      password: "",
+      error: ""
     }
   }
 
-  login(){
-    if(!this.state.account || !this.state.password){
-        this.setState({
-            error: "Please fill the Account and Password."
-        })
-        return;
+  login() {
+    if (!this.state.account || !this.state.password) {
+      this.setState({
+        error: "Please fill the Account and Password."
+      })
+      return;
     }
 
     //1. Assemble user data
     let user = {
-      account:this.state.account,
-      password:this.state.password
+      account: this.state.account,
+      password: this.state.password
     };
 
     //2. Call API, adduUser
-    axios.post('/login/login', {user: user})
+    axios.post('/login/login', { user: user })
       .then((response) => {
         this.props.history.push('/');
       })
       .catch((error) => {
         this.setState({
-            error: error.response.data.res_body
+          error: error.response.data.res_body
         })
       })
   }
 
-  back(){
+  back() {
     this.props.history.push('/');
   }
 
@@ -48,16 +48,16 @@ class LoginPage extends React.Component {
     return (
       <div className="login-content">
         <div className="warning">
-            {this.state.error}
+          {this.state.error}
         </div>
         <table>
           <tbody>
             <tr>
               <td><label>Account</label></td>
               <td>:</td>
-              <td> 
-                <input type="text" value={this.state.account} 
-                  onChange={e => this.setState({account:e.target.value})}>
+              <td>
+                <input type="text" value={this.state.account}
+                  onChange={e => this.setState({ account: e.target.value })}>
                 </input>
               </td>
             </tr>
@@ -65,37 +65,33 @@ class LoginPage extends React.Component {
               <td><label>Password</label></td>
               <td>:</td>
               <td>
-                <input type="password" value={this.state.password} 
-                  onChange={e => this.setState({password:e.target.value})}>
+                <input type="password" value={this.state.password}
+                  onChange={e => this.setState({ password: e.target.value })}>
                 </input>
-              </td>
-            </tr>
-            <tr>
-              <td/>
-              <td/>
-              <td>
-                <button className='button' onClick={()=> this.back()}>Back</button>
-                <button className='button' onClick={()=> this.login()}>Login</button>
               </td>
             </tr>
           </tbody>
         </table>
+        <div className="buttons-grid">
+          <button className='button' onClick={() => this.login()}>Login</button>
+          <button className='button' onClick={() => this.back()}>Back</button>
+        </div>
       </div>
     )
   }
 }
 
-let mapDispatchToProps = function(dispatch, props) {
-    return {
-    }
+let mapDispatchToProps = function (dispatch, props) {
+  return {
+  }
 }
-  
-let mapStateToProps = function(state, props) {
-    return {
-    }
+
+let mapStateToProps = function (state, props) {
+  return {
+  }
 }
-  
+
 export default connect(
-    mapStateToProps,
-    mapDispatchToProps
+  mapStateToProps,
+  mapDispatchToProps
 )(LoginPage);
